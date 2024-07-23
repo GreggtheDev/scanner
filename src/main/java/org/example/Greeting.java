d62d5a88d95f7ed1dbe150b5fe0faf1bdb9ef5c8
GreggtheDev <greggthedev@gmail.com>
2024-07-23 13:10:05 -0400

    initial commit.

---
+++ src/main/java/org/example/Greeting.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
+    }
+}
+
